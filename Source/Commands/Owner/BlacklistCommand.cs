4e361078e9bc9d6499b1d0033c189a8c0ff3d28c
Frankie B <floppydisk05@aol.com>
2022-04-18 19:18:22 -0400

    Revert blacklist to permissions-based

--- Source/Commands/Owner/BlacklistCommand.cs
+++ Source/Commands/Owner/BlacklistCommand.cs
@@ -23,10 +23,10 @@ namespace WinBot.Commands.Staff
         [RequireOwner]
         public async Task Blacklist(CommandContext Context, DiscordMember user = null)
         {
-            //if(!PermissionMethods.HasPermission(Context.Member.PermissionsIn(Context.Channel), Permissions.KickMembers) && Context.Member.Id != Bot.client.CurrentApplication.Owners.FirstOrDefault().Id)
-            //    return;
-            if (!(Context.User.Id == 437970062922612737) && !(Context.User.Id == 584532167002947604) && !(Context.User.Id == 404817718542598154) && !(Context.User.Id == 256033911438573568) && !(Context.User.Id == 447380526798471170))
+            if(!PermissionMethods.HasPermission(Context.Member.PermissionsIn(Context.Channel), Permissions.KickMembers) && Context.Member.Id != Bot.client.CurrentApplication.Owners.FirstOrDefault().Id)
                 return;
+            //if (!(Context.User.Id == 437970062922612737) && !(Context.User.Id == 584532167002947604) && !(Context.User.Id == 404817718542598154) && !(Context.User.Id == 256033911438573568) && !(Context.User.Id == 447380526798471170))
+            //    return;
 
             // This is a clunky method of listing things but it works
             if(user == null) {
