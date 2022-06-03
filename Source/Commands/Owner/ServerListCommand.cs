4571e2ca5c0352a423ceb18726f76ada59edb133
Frankie B <floppydisk05@aol.com>
2022-06-03 18:39:32 +0100

    Add serverlist command

---
+++ Source/Commands/Owner/ServerListCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+using WinBot.Commands.Attributes;
+
+using WinBot.Misc;
+using WinBot;
+
+using Serilog;
+
+namespace WinBot.Commands.Owner
+{
+    public class ServerListCommand : BaseCommandModule
+    {
+        [Command("serverlist")]
+        [Description("Lists guilds the bot is in")]
+        [Category(Category.Owner)]
+        [RequireOwner]
+        public async Task ServerList(CommandContext Context)
+        {
+            DiscordEmbedBuilder Embed = new DiscordEmbedBuilder();
+            Embed.WithColor(DiscordColor.Gold);
+            string output = "";
+            foreach (var guild in Bot.client.Guilds) {
+                output += $"{guild.Value.Members}\n";
+            }
+            Embed.WithTitle("Servers");
+            Embed.WithDescription(output);
+            await Context.ReplyAsync("", Embed.Build());
+        }
+    }
+}
