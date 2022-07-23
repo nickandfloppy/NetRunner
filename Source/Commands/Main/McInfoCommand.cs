4fe56d2633a7817c2886e2ca4c05235bc6ef6da5
Frankie B <floppydisk05@aol.com>
2022-07-23 21:05:45 +0100

    Update addresses in mcinfo

--- Source/Commands/Main/McInfoCommand.cs
+++ Source/Commands/Main/McInfoCommand.cs
@@ -23,15 +23,15 @@ namespace WinBot.Commands.Main
             string dynmap = "";
             string versions = "";
             if (Context.Guild.Id == 764493398983049246) {
-                server = "mc.nick99nack.com";
-                dynmap = "http://mc.nick99nack.com/";
+                server = "mc.nickandfloppy.com";
+                dynmap = "http://mc.nickandfloppy.com/";
                 versions = "1.7.2 -> 1.16.5";
             } else if (Context.Guild.Id == 955969771994742874) {
                 server = "hiden.pw";
                 dynmap = "https://mc.hiden.pw/";
                 versions = "1.7.x -> 1.19.x";
             } else if (Context.Guild.Id == 936271948927881276) {
-                server = "mc.nick99nack.com:25560";
+                server = "mc.nickandfloppy.com:25560";
                 versions = "1.16.5";
             }else
                 throw new Exception("This server does not have a server configured");
