7fce4e183c8792db1cc8eb5d73697acba5d6039a
dkay0670 <63021402+dkay0670@users.noreply.github.com>
2022-12-31 22:22:48 -0800

    norton 2.0
    Signed-off-by: dkay0670 <63021402+dkay0670@users.noreply.github.com>

--- Source/Commands/Main/YearCommand.cs
+++ Source/Commands/Main/YearCommand.cs
@@ -10,12 +10,12 @@ namespace WinBot.Commands.Main
 {
     public class YearCommand : BaseCommandModule
     {
-        [Command("2022")]
-        [Description("Gets the remaining time in 2022")]
+        [Command("year")]
+        [Description("Gets the remaining time in the year")]
         [Category(Category.Fun)]
-        public async Task Ping(CommandContext Context)
+        public async Task Year(CommandContext Context)
         {
-            var timeSpan = new DateTime(2023, 1, 1).Subtract(DateTime.Now);
+            var timeSpan = new DateTime(DateTime.Now.Year + 1, 1, 1).Subtract(DateTime.Now);
             await Context.ReplyAsync($"There are {Math.Round((double)timeSpan.TotalDays,2)} days left in 2022. That's {Math.Round((double)timeSpan.TotalHours,2)} hours, {Math.Round((double)timeSpan.TotalMinutes,2)} minutes, or {Math.Round((double)timeSpan.TotalSeconds,2)} seconds.");
         }
     }
