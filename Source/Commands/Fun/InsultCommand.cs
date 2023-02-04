03d6f111622dba6f8b7618978564193f3160d305
HIDEN64 <hiden64@protonmail.com>
2023-02-03 21:35:47 -0600

    add an insult command
    because funny i guess

---
+++ Source/Commands/Fun/InsultCommand.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+using WinBot.Commands.Attributes;
+
+using Newtonsoft.Json;
+
+namespace WinBot.Commands.fun {
+	public class InsultCommand : BaseCommandModule {
+        [Command("insult")]
+		[Description("Insults you. Fun.")]
+		[Usage("insult")]
+		[Category(Category.Fun)]
+		public async Task Insult(CommandContext Context) {
+			DiscordEmbedBuilder eb = new DiscordEmbedBuilder();
+            // Get the thing
+            string json = "";
+            using(HttpClient http = new HttpClient())
+                json = await http.GetStringAsync($"http://evilinsult.com/generate_insult.php?lang=en&type=json");
+
+            dynamic insultText = JsonConvert.DeserializeObject(json);  // Deserialize the thang
+				// Send the thing
+				eb.WithTitle("You got insulted. Ha...");
+				eb.WithColor(new DiscordColor("#02cc09"));
+                eb.WithDescription($"{insultText.insult}");
+		
+			await Context.ReplyAsync("", eb.Build());
+		}
+	}
+}
--- Source/Commands/Main/McInfoCommand.cs
+++ Source/Commands/Main/McInfoCommand.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net;
 using System.Threading.Tasks;
 
-
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
