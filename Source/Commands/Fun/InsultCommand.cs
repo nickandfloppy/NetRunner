320c2c91e2ea8a3fee324a78f498230e453ac82c
Frankie B <git@diskfloppy.me>
2023-02-06 13:11:52 +0000

    Fix indentation

--- Source/Commands/Fun/InsultCommand.cs
+++ Source/Commands/Fun/InsultCommand.cs
@@ -10,25 +10,25 @@ using WinBot.Commands.Attributes;
 using Newtonsoft.Json;
 
 namespace WinBot.Commands.fun {
-	public class InsultCommand : BaseCommandModule {
+    public class InsultCommand : BaseCommandModule {
         [Command("insult")]
-		[Description("Insults you. Fun.")]
-		[Usage("insult")]
-		[Category(Category.Fun)]
-		public async Task Insult(CommandContext Context) {
-			DiscordEmbedBuilder eb = new DiscordEmbedBuilder();
+        [Description("Insults you. Fun.")]
+        [Usage("insult")]
+        [Category(Category.Fun)]
+        public async Task Insult(CommandContext Context) {
+            DiscordEmbedBuilder eb = new DiscordEmbedBuilder();
             // Get the thing
             string json = "";
             using(HttpClient http = new HttpClient())
-                json = await http.GetStringAsync($"http://evilinsult.com/generate_insult.php?lang=en&type=json");
+            json = await http.GetStringAsync($"http://evilinsult.com/generate_insult.php?lang=en&type=json");
 
             dynamic insultText = JsonConvert.DeserializeObject(json);  // Deserialize the thang
-                // Send the thing
-                eb.WithTitle("You got insulted. Ha...");
-                eb.WithColor(new DiscordColor("#02cc09"));
-                eb.WithDescription($"{insultText.insult}");
-		
-			await Context.ReplyAsync("", eb.Build());
-		}
-	}
+            // Send the thing
+            eb.WithTitle("You got insulted. Ha...");
+            eb.WithColor(new DiscordColor("#02cc09"));
+            eb.WithDescription($"{insultText.insult}");
+
+            await Context.ReplyAsync("", eb.Build());
+        }
+    }
 }
