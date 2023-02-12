202cb67c258bed9954bd45950c49ab41b0a8c497
HIDEN64 <hiden64@protonmail.com>
2023-02-12 00:39:57 -0600

    fix git bullshit

--- Source/Commands/Fun/InsultCommand.cs
+++ Source/Commands/Fun/InsultCommand.cs
@@ -23,7 +23,6 @@ namespace WinBot.Commands.fun {
             json = await http.GetStringAsync($"http://evilinsult.com/generate_insult.php?lang=en&type=json");
 
             dynamic insultText = JsonConvert.DeserializeObject(json);  // Deserialize the thang
-<<<<<<< HEAD
                 // Send the thing
                 eb.WithTitle("Get insulted... loser.");
                 eb.WithColor(DiscordColor.Gold);
@@ -32,14 +31,4 @@ namespace WinBot.Commands.fun {
 			await Context.ReplyAsync("", eb.Build());
 		}
 	}
-=======
-            // Send the thing
-            eb.WithTitle("You got insulted. Ha...");
-            eb.WithColor(new DiscordColor("#02cc09"));
-            eb.WithDescription($"{insultText.insult}");
-
-            await Context.ReplyAsync("", eb.Build());
-        }
-    }
->>>>>>> d5b1fad891e37209e9c8ed8652f13192ab55bd1d
 }
