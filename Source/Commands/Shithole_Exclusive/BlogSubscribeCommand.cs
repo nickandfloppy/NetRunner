0ea8c55cd43b8dc14e66e25760e2fbc953d3b4b9
HIDEN64 <hiden64@protonmail.com>
2023-02-13 03:19:10 -0600

    add the h

---
+++ Source/Commands/Shithole_Exclusive/BlogSubscribeCommand.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using HBot.Commands.Attributes;
+
+namespace HBot.Commands.Shithole_Exclusive {
+    public class BlogSubscribeCommand : BaseCommandModule {
+        private const long ShitholeId = 955969771994742874;
+
+        [Command("voting-sub")]
+        [Description("Subscribes or unsubscribes you to pings for polls")]
+        [Category(Category.Shithole_Exclusive)]
+        public async Task Subscribe(CommandContext ctx) {
+            
+            if (ctx.Guild.Id != ShitholeId)
+                throw new System.Exception("This command can only be run in HIDEN's Shithole.");
+
+            var role = ctx.Guild.GetRole(1074620373385097216);
+
+            if (ctx.Member.Roles.Contains(role)) {
+                await ctx.Member.RevokeRoleAsync(role);
+                await ctx.RespondAsync("Unsubscribed from polling pings.");
+                return;
+            }
+
+            await ctx.Member.GrantRoleAsync(role);
+            await ctx.RespondAsync("Succesfully subscribed to polling pings!");
+        }
+    }
+}
