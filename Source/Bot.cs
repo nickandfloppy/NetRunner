6e3b4ed1b28c93a2673ed9f46315ea164bb5b4df
HIDEN64 <hiden64@protonmail.com>
2023-04-11 07:10:12 -0500

    bump version number

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -28,7 +28,7 @@ using ImageMagick;
 
 namespace HBot {
     class Bot {
-        public const string VERSION = "1.5.1";
+        public const string VERSION = "1.5.2";
 
         public static void Main(string[] args) => new Bot().RunBot().GetAwaiter().GetResult();
 
