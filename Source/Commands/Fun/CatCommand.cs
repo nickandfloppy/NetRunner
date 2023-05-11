75caf89f693065a672f840f34f12e58c89e7d2f0
HIDEN64 <hiden64@protonmail.com>
2023-05-11 07:48:54 -0500

    rewrite cat to use my own api

--- Source/Commands/Fun/CatCommand.cs
+++ Source/Commands/Fun/CatCommand.cs
@@ -1,14 +1,11 @@
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-
 using HBot.Commands.Attributes;
-
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HBot.Commands.Fun {
     public class CatCommand : BaseCommandModule {
@@ -17,19 +14,33 @@ namespace HBot.Commands.Fun {
         [Category(Category.Fun)]
         public async Task Cat(CommandContext Context) {
             string json = "";
-            // Download the json string from the API
-            using(HttpClient http = new HttpClient())
-            json = await http.GetStringAsync($"https://api.thecatapi.com/v1/images/search?api_key={Bot.config.apiKeys.catAPIKey}");
-            dynamic output = JsonConvert.DeserializeObject(json); // Deserialize the string into a dynamic object
+            using (HttpClient http = new HttpClient()) {
+                try {
+                    json = await http.GetStringAsync("http://localhost/api/hbot/random-cat");
+                }
+                catch (Exception ex) {
+                    await Context.Channel.SendMessageAsync($"Error fetching cat image: {ex.Message}");
+                    return;
+                }
+            }
 
-            // Create and send the embed
-            var eb = new DiscordEmbedBuilder();
-            eb.WithColor(new DiscordColor("#007FFF"));
-            eb.WithTitle("Here's your cat!");
-            eb.WithImageUrl((string)output[0].url);
-            eb.WithTimestamp(DateTime.Now);
-            await Context.ReplyAsync("", eb.Build());
-            await Task.Delay(1);
+            JObject data = JObject.Parse(json);
+            if (data == null || string.IsNullOrEmpty(data.Value<string>("img_url"))) {
+                await Context.Channel.SendMessageAsync("Error fetching cat image: the API shat likely itself");
+                return;
+            }
+
+            try {
+                byte[] imageData = Convert.FromBase64String(data.Value<string>("img_url").Split(',')[1]);
+                using (var ms = new MemoryStream(imageData)) {
+                    var attachment = new DSharpPlus.Entities.DiscordMessageBuilder().WithFile("cat.jpg", ms);
+                    await Context.Channel.SendMessageAsync(attachment); //Have to remove the "Here's your cat!" message for now because of DPP bullshittery
+                }
+            }
+            catch (Exception ex) {
+                await Context.Channel.SendMessageAsync($"Error sending cat image: {ex.Message}");
+                return;
+            }
         }
     }
 }
